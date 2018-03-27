8e91d8976d9e5ba774dfc241e33f7ba20a32c8de
Jared Lutz and Luke Stockburger <jared@>
2018-03-27 11:41:51 -0700

    make a word puzzle

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var answer = "hello whats going on";
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var puzzle = ""
+  for (var index = 0; index <= vowels.length; index += 1) {
+    if index.charAt(vowels){
+      voweltoreplace = index.replace(index, "-");
+    };
+
+  }
+  for (var index = 0; index <= answer.charAt[index]; index += 1) {
+  }
+  alert("voweltoreplace");
+});
