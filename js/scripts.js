ce63267f14fcdfaa5d4800e6f857e32fea530423
jared <jaredspecjr@gmail.com>
2018-03-27 18:50:00 -0700

    change if statement to DRY up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   var sentence = "Hello world how are you?";
-  var vowels = ["a", "e", "i", "o", "u"]
+  var vowels = ["a", "e", "i", "o", "u"];
   for (index = 0; index < sentence.length; index ++) {
-    if (sentence[index] === "a" || sentence[index] === "e" || sentence[index] === "i" || sentence[index] === "o" || sentence[index] === "u") {
+    if (vowels.includes(sentence[index])) {
       sentence = sentence.replace(sentence[index], "-");
-      console.log(sentence);
+
     }
-
-
   }
+  $("#answer").append(sentence);
 
 });
